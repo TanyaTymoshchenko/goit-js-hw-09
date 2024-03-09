c0c6afeeb3bde932cb2b186a9115893ab19b2ee3
TanyaTymoshchenko <ttymoshchenko@yahoo.com>
2024-03-09 21:22:39 +0200

    update

---
+++ src/js/1-gallery.js
@@ -0,0 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from './gallery-images';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryHTML = images
+  .map(
+    image => `
+  <li class="gallery-item">
+  <a class="gallery-link" href="${image.original}">
+    <img
+      class="gallery-image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryHTML);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
